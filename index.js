ff9594c4cd5df50edcafd57b5a6e6dce1dd99786
aonetecha <85815644+aonetecha@users.noreply.github.com>
2022-05-16 08:08:26 +0100

    ./

--- index.js
+++ index.js
@@ -1,24 +1,44 @@
-let screen = document.getElementById('speechtotext');
-val = document.getElementById('shear_bar');
+let mic_btn = document.getElementById('speechtotext');
+search_bar = document.getElementById('search_bar');
 
-screen.addEventListener('click', () => {
+mic_btn.addEventListener('click', () => {
   // Use API
   var recognition = new webkitSpeechRecognition || window.SpeechRecognition;
 
   recognition.lang = "en-GB"
   recognition.onresult = function (event) {
-    val.value = event.results[0][0].transcript;
+    search_bar.value = event.results[0][0].transcript;
   }
   // start record
   recognition.start();
 
   // Search your query in google
   setTimeout(() => {
-    if (val.value == "") {
+    if (search_bar.value == "") {
     } else {
-      window.location = "http://www.google.com/search?q=" + val.value;
+      window.location = "http://www.google.com/search?q=" + search_bar.value;
       return false;
     }
   }, 5000);
 });
+
+// Type text automatically in input placeholder 
+var i = 0;
+var txt = "Search Google ot type a URL";
+
+if (location.reload) {
+  types();
+}
+function types() {
+  if (i < txt.length) {
+    document.getElementById('search_bar').placeholder += txt.charAt(i);
+    i++;
+  }
+  setTimeout(types, 100);
+}
+
+
+
+
+
 
